a3cf18f0f908a27121677ef71b843f490f5bd338
ari-fatur <ari.arifatur.fatur@gmail.com>
2023-07-15 15:14:13 +0700

    penambahan auto remove cache

--- src/router.rs
+++ src/router.rs
@@ -3,13 +3,11 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use axum::extract::{Path, State};
-use axum::http::{ HeaderName};
-
+use axum::http::HeaderName;
 use axum::Router;
 use axum::routing::get;
 use dotenvy::var;
 use reqwest::{Client, header};
-use tokio::io::AsyncWriteExt;
 
 use crate::error::MyError;
 
@@ -55,45 +53,34 @@ async fn cdn(State(client): State<Arc<Client>>, Path(url): Path<String>) -> Resu
             }
         }
         Err(_) => {
-            let image_webp = new_data(&client, &url, &path).await;
+            let image_webp = new_data(&client, &url, path).await;
             if image_webp.is_err() {
                 return Err(MyError::InternalServerError);
             }
             let image_webp = image_webp.unwrap();
-            return Ok(([(header::CONTENT_TYPE, "image/webp".to_string()), (header::CACHE_CONTROL, "public, max-age=604800".to_string())],
+            return Ok(([(header::CONTENT_TYPE, "image/avif".to_string()), (header::CACHE_CONTROL, "public, max-age=604800".to_string())],
                        image_webp));
         }
     }
     Err(MyError::BadRequest)
 }
 
-async fn save_image_from_bytes(bytes: Vec<u8>, file_path: PathBuf) -> std::io::Result<()> {
-    let mut file = tokio::fs::File::create(file_path).await?;
-    file.write_all(&bytes).await?;
-    Ok(())
-}
 
-async fn convert_webp(data: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
-    let a = image::load_from_memory(&data)?;
-    let b = webp::Encoder::from_image(&a)?;
-    let c = b.encode(75.0);
-    return Ok(c.to_vec());
-}
-
-async fn new_data(client: &Client, url: &str, path: &PathBuf) -> Result<Vec<u8>, Box<dyn Error>> {
+async fn new_data(client: &Client, url: &str, path: PathBuf) -> Result<Vec<u8>, Box<dyn Error>> {
     let resp = client.get(url).send().await?;
-    let body = resp.bytes().await?.to_vec();
+    let body = resp.bytes().await?;
 
-    let body = convert_webp(body).await?;
-
-    let body2 = body.clone();
-    let path = path.clone();
-
-    tokio::spawn(async move {
-        let _ = save_image_from_bytes(body2, path).await;
-    });
-
-    Ok(body)
+    //convert
+    let s = std::time::Instant::now();
+    let img = image::load_from_memory(&body)?;
+    let convert = img.save_with_format(&path, image::ImageFormat::WebP);
+    if convert.is_err() {
+        let _ = std::fs::remove_file(&path);
+    }
+    let a = std::fs::read(path)?;
+    let e = std::time::Instant::now();
+    println!("{:?}", e - s);
+    Ok(a)
 }
 
 #[tokio::test]
